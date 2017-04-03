780d94c57c488c4c948d9df902669ff0324474d2
bennerv <bves94@gmail.com>
2017-04-03 15:46:02 -0400

    New threading for reentrancy delay and other message delay.  Hopefully it works :D

--- wear/src/main/java/com/ford/ev_coach/VibrationListenerService.java
+++ wear/src/main/java/com/ford/ev_coach/VibrationListenerService.java
@@ -1,5 +1,6 @@
 package com.ford.ev_coach;
 
+import android.icu.util.TimeUnit;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Vibrator;
@@ -16,105 +17,111 @@ public class VibrationListenerService extends WearableListenerService {
     private static final int MINUTE_DELAY_TIME = 10;
 
     /* Reentrant and delay booleans */
-    private boolean reentrant = true;
-    private boolean speedDelay = false;
-    private boolean rpmDelay = false;
-    private boolean accelDelay = false;
+    private boolean mReentrant = true;
+    private boolean mSpeedDelay = false;
+    private boolean mRpmDelay = false;
+    private boolean mAccelDelay = false;
 
-    private Handler mHandler = new Handler();
-
-    private Runnable mRunnable = new Runnable() {
-        @Override
-        public void run() {
-            reentrant = !reentrant;
-            Log.d(TAG, "Reentrancy? " + reentrant);
-            mHandler.postDelayed(mRunnable, 10000);
-            if(reentrant) mHandler.removeCallbacks(mRunnable);
-        }
-    };
-
-    /* Delay for speed message to reoccur */
-    private Runnable mSpeedRunnable = new Runnable() {
-        @Override
-        public void run() {
-            speedDelay = !speedDelay;
-            Log.d(TAG, "Speed delay " + speedDelay);
-            mHandler.postDelayed(mSpeedRunnable, MINUTE_DELAY_TIME * 60 * 1000);
-            if(!speedDelay) mHandler.removeCallbacks(mSpeedRunnable);
-        }
-    };
-
-    /* Delay for rpm message to reoccur */
-    private Runnable mRpmRunnable = new Runnable() {
-        @Override
-        public void run() {
-            rpmDelay = !rpmDelay;
-            Log.d(TAG, "Rpm delay " + rpmDelay);
-            mHandler.postDelayed(mRpmRunnable, MINUTE_DELAY_TIME * 60 * 1000);
-            if(!rpmDelay) mHandler.removeCallbacks(mRpmRunnable);
-        }
-    };
-
-    /* Delay for accel message to reoccur */
-    private Runnable mAccelRunnable = new Runnable() {
-        @Override
-        public void run() {
-            accelDelay = !accelDelay;
-            Log.d(TAG, "Accel delay " + accelDelay);
-            mHandler.postDelayed(mAccelRunnable, MINUTE_DELAY_TIME * 60 * 1000);
-            if(!accelDelay) mHandler.removeCallbacks(mAccelRunnable);
-        }
-    };
+    /* Handlers for threads */
+    private Handler mReentrantHandler = new Handler();
+    private Handler mSpeedHandler = new Handler();
+    private Handler mRpmHandler = new Handler();
+    private Handler mAccelHandler = new Handler();
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
 
-        // Wait at least 10 seconds before running this after a prior message throwing out older messages
-        while(!reentrant) {
-            int num = messageEvent.getData().length > 0 ? messageEvent.getData()[0] : -1;
-            if(num == 0 && rpmDelay) return;
-            else if(num == 1 && speedDelay) return;
-            else if(num == 3 && accelDelay) return;
-        }
-
-        mRunnable.run();
-
         Log.d(TAG, "Message received from " + messageEvent.getSourceNodeId());
-        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 
         // Get the specified number out of the message 0-RPM 1-VehicleSpeed 2-Acceleration
         byte num = messageEvent.getData().length > 0 ? messageEvent.getData()[0] : -1;
 
+        // Handle a vibration message (synchronized method)
+        handleVibration(num);
+
+        super.onMessageReceived(messageEvent);
+    }
+
+
+    private synchronized void handleVibration(byte num) {
+
+        // Return if we are waiting on delays already
+        if(num == 0 && mRpmDelay) return;
+        else if(num == 1 && mSpeedDelay) return;
+        else if(num == 3 && mAccelDelay) return;
+
+        // 10 second reentrant delay between messages
+        while(!mReentrant);
+
+        // 10 second reentrant delay
+        new Runnable() {
+            @Override
+            public void run() {
+                mReentrant = !mReentrant;
+
+                // If we can reenter after 10 seconds, return otherwise we want to delay 10 seconds
+                if(!mReentrant) {
+                    mReentrantHandler.postDelayed(this, 10000);
+                }
+            }
+        }.run();
+
+        // Vibration service
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+        // Vibration portion with delays
         if(vibrator.hasVibrator()) {
 
             // Different vibration pattern depending on the message contents
             switch(num) {
                 case 0:
-                    if(rpmDelay) return;
-                    mRpmRunnable.run();
                     Log.d(TAG, "RPM Message");
                     vibrator.vibrate(new long[] {0, 750, 500, 750}, -1);  // long[] {how long to wait before starting, how long to vibrate for, ...}
-                    Toast.makeText(getApplicationContext(), "RPM", Toast.LENGTH_LONG).show();
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            mRpmDelay = !mRpmDelay;
+                            Toast.makeText(getApplicationContext(), "RPM Warning", Toast.LENGTH_SHORT).show();
+                            if(mRpmDelay) {
+                                mRpmHandler.postDelayed(this, MINUTE_DELAY_TIME * 60000);
+                            }
+                        }
+                    }.run();
                     break;
                 case 1:
-                    if(speedDelay) return;
-                    mSpeedRunnable.run();
                     Log.d(TAG, "Speed Message");
                     vibrator.vibrate(new long[] {0, 2000}, -1);
-                    Toast.makeText(getApplicationContext(), "Speed", Toast.LENGTH_LONG).show();
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            mSpeedDelay = !mSpeedDelay;
+                            Toast.makeText(getApplicationContext(), "Speed Warning", Toast.LENGTH_SHORT).show();
+                            if(mSpeedDelay) {
+                                mSpeedHandler.postDelayed(this, MINUTE_DELAY_TIME * 60000);
+                            }
+                        }
+                    }.run();
+
                     break;
                 case 2:
-                    if(accelDelay) return;
-                    mAccelRunnable.run();
                     Log.d(TAG, "Accel Message");
                     vibrator.vibrate(new long[] {0, 750, 500, 750, 500, 750}, -1);
-                    Toast.makeText(getApplicationContext(), "Accel", Toast.LENGTH_LONG).show();
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            mAccelDelay = !mAccelDelay;
+                            Toast.makeText(getApplicationContext(), "Accel Warning", Toast.LENGTH_SHORT).show();
+                            if(mAccelDelay) {
+                                mAccelHandler.postDelayed(this, MINUTE_DELAY_TIME * 60000);
+                            }
+                        }
+                    }.run();
+
                     break;
                 default:
-                    //vibrator.vibrate(100);
+                    Log.d(TAG, "Invalid message id");
                     break;
             }
         }
-        super.onMessageReceived(messageEvent);
     }
 }
