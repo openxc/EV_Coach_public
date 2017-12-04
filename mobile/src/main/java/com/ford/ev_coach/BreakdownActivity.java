ab2de2aeae10bf84589e6bc285f4903d104bb625
Brandon <bnfronce@mtu.edu>
2017-12-03 19:57:56 -0500

    Firebase Functionality

--- mobile/src/main/java/com/ford/ev_coach/BreakdownActivity.java
+++ mobile/src/main/java/com/ford/ev_coach/BreakdownActivity.java
@@ -6,9 +6,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class BreakdownActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    static FirebaseDatabase database = FirebaseDatabase.getInstance();
+    static FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    static DatabaseReference ref = database.getReference("users/" + user.getUid() + "/");
+    static DatabaseReference scoreRef = ref.child("scores");
+    static Map<String, String> scores = new HashMap<>();
     private Button loginButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +55,14 @@ public class BreakdownActivity extends AppCompatActivity implements View.OnClick
 
         TextView score = (TextView) findViewById(R.id.score);
         score.setText(totalScore + " / 1000.00");
+
+
+        scores.put("MPG", MPGScore);
+        scores.put("Accel Score", accelScore);
+        scores.put("RPM Score", RPMscore);
+        scores.put("Speed Score", speedScore);
+        scores.put("Grade", Grade);
+        scores.put("Total Score", totalScore);
     }
 
     @Override
@@ -52,4 +72,5 @@ public class BreakdownActivity extends AppCompatActivity implements View.OnClick
             startActivity(intent);
         }
     }
+
 }
