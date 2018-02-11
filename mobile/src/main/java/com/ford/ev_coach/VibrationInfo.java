9e75d524e9bd8d0b382a5882de2a8732b6f1c65c
andrew <asfisher@mtu.edu>
2018-02-11 15:41:09 -0500

    Merge branch 'master' of https://github.com/fisherandrew04/EV_Coach
    # Conflicts:
    #	build.gradle

---
+++ mobile/src/main/java/com/ford/ev_coach/VibrationInfo.java
@@ -0,0 +1,16 @@
+package com.ford.ev_coach;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by andrew on 12/3/17.
+ */
+
+public class VibrationInfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vibration_info_activity);
+    }
+}
